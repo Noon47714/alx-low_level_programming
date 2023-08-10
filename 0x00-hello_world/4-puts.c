dffed33f2e0133d12761bf555d0c6ca6cd307bb2
Noon47714 <noon47714@gmail.com>
2023-08-10 10:04:26 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
  * main - A promgram that prints a line with puts function
  * Return: 0 (Success)
  */
-int mani(void)
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
