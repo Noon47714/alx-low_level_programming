b00512793ec40a14c0e29294c8df0d634ff1b650
Noon47714 <noon47714@gmail.com>
2023-08-24 05:16:13 -0700

    task0: git commit -m task0
    git commit -m task0:

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,26 +1,29 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * _strcat - Concatenates two string
+ * *_strcat - Concatenates two string
  * @dest: the string
+ * @src: input value
  * Return: A pointer to the resulting string dest
  */
 
 char *_strcat(char *dest, char *src)
 {
-	int dlen = 0, i;
-	while (dest[dlen])
+	int i;
+	int j;
 
+	i = 0;
+	while (dest[i] != '\0')
 	{
-		dlen++;
+		i++;
 	}
-
-	for (i = 0; src[i] != 0; i++)
+	j = 0;
+	while (src[j] != '\0')
 	{
-		dest[dlen] = src[i];
-		dlen++;
+		dest[i] = src[j];
+		i++;
+		j++;
 	}
-	dest[dlen] = '\0';
+	dest[i] = '\0';
 	return (dest);
 }
