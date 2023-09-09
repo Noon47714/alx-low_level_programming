2904f0151eec61a28ad472f5c5280abcb4a30352
Noon47714 <noon47714@gmail.com>
2023-09-09 04:17:24 -0700

    task

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,14 +2,14 @@
 
 /**
  * _isupper - function that verifies if a  character upper
- * @c: character
- * Return: returns 1 if it is uppercase 0 if not.
+ * @c: char to be checked
+ * Return: 0 or 1
  */
 
-int _isdigit(int c)
+int _isupper(int c)
 {
 	if ((c >= 'A') && (c <= 'Z'))
 		return (1);
-
-	return (0);
+	else
+		return (0);
 }
