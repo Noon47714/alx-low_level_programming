c6f9dc233241b4ad61a2b24b4c45fea949651925
Noon47714 <noon47714@gmail.com>
2023-09-24 12:41:27 -0700

    task0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: size of the memory
+ * Return: pointer to the address of the memory
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *block;
+
+	block = malloc(b);
+	if (block == NULL)
+		exit(98);
+	return (block);
+}
