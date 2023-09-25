ea6291120c5c2fea8867a4815d44eb60da226065
Noon47714 <noon47714@gmail.com>
2023-09-25 11:20:52 -0700

    task

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * listint_len - Returns the number of elements
+ * @h: A pointer to the head of the list
+ * Return: the number of elements
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t nodes = 0;
+
+	while (h)
+	{
+		nodes++;
+		h = h->next;
+	}
+	return (nodes);
+}
