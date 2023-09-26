097fe74d30828c20bd530c0115f14aca63875086
Noon47714 <noon47714@gmail.com>
2023-09-26 02:50:19 -0700

    task

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,104 +1,108 @@
 #include "main.h"
+#include <stdio.h>
+#include <string.h>
 
 /**
- * main - function that multiplies two positive numbers.
- * @argc: number of arguments.
- * @argv: pointer to array of arguments.
- * Print the result, followed by a new line.
- * If the number of arguments is incorrect, print Error, followed by a,
- * new line, and exit with a status of 98.
- * num1 and num2 should only be composed of digits. If not, print Error,
- * followed by a new line, and exit with a status of 98.
- * You are allowed to use more than 5 functions in your file.
- * Return: always 0 (Success)
+ * _isdigit - checks if character
+ * @c: the character to check
+ * Return: 1 if digit, 0 otherwise
  */
-int main(int argc, char *argv[])
+int _isdigit(int c)
 {
-	char *str1, *str2;
-	int length1, length2, length, i, carry, digit1, digit2, *result, a = 0;
-
-	str1 = argv[1], str2 = argv[2];
-	if (argc != 3 || !is_digit(str1) || !is_digit(str2))
-		errors();
-	length1 = _strlen(str1);
-	length2 = _strlen(str2);
-	length = length1 + length2 + 1;
-	result = malloc(sizeof(int) * length);
-	if (!result)
-		return (1);
-
-	for (i = 0; i <= length1 + length2; i++)
-		result[i] = 0;
-
-	for (length1 = length1 - 1; length1 >= 0; length1--)
-	{
-		digit1 = str1[length1] - '0';
-
-		carry = 0;
-
-		for (length2 = _strlen(str2) - 1; length2 >= 0; length2--)
-		{
-			digit2 = str2[length2] - '0';
-			carry += result[length1 + length2 + 1] + (digit1 * digit2);
-			result[length1 + length2 + 1] = carry % 10;
-			carry /= 10;
-		}
-		if (carry > 0)
-			result[length1 + length2 + 1] += carry;
-	}
-	for (i = 0; i < length - 1; i++)
-	{
-		if (result[i])
-			a = 1;
-		if (a)
-			_putchar(result[i] + '0');
-	}
-	if (!a)
-		_putchar('0');
-	_putchar('\n');
-	free(result);
-	return (0);
+	return (c >= '0' && c <= '9');
 }
 
 /**
- * is_digit - function that checks if a string contains a non-digit char.
- * @str: pointer to null-terminated byte string to be examined.
- * Return: 0 if a non-digit is found, 1 otherwise
+ * _strlen - returns the length
+ * @s: the string whose length to check
+ * Return: integer length
  */
-int is_digit(char *str)
+int _strlen(char *s)
 {
 	int i = 0;
 
-	while (str[i])
-	{
-		if (str[i] < '0' || str[i] > '9')
-			return (0);
+	while (*s++)
 		i++;
-	}
-	return (1);
-}
-
-/**
- * _strlen - function that returns the length of a string.
- * @str: pointer to null-terminated byte string to be examined.
- * Return: The length of the string.
- */
-int _strlen(char *str)
-{
-	int i = 0;
-	
-	while (str[i] != '\0')
-	{
-		i++;
-	}
 	return (i);
 }
 
 /**
- * errors - function that handles errors for main.
+ * big_multiply - multiply two big number strings
+ * @s1: the first big number string
+ * @s2: the second big number string
+ * Return: the product big number
  */
-void errors(void)
+char *big_multiply(char *s1, char *s2)
 {
-	printf("Error\n");
-	exit(98);
+	char *r;
+	int l1, l2, a, b, c, x;
+
+	l1 = _strlen(s1);
+	l2 = _strlen(s2);
+	r = malloc(a = x = l1 + l2);
+	if (!r)
+		printf("Error\n"), exit(98);
+	while (a--)
+		r[a] = 0;
+
+	for (l1--; l1 >= 0; l1--)
+	{
+		if (!_isdigit(s1[l1]))
+		{
+			free(r);
+			printf("Error\n"), exit(98);
+		}
+		a = s1[l1] - '0';
+		c = 0;
+		for (l2 = _strlen(s2) - 1; l2 >= 0; l2--)
+		{
+			if (!_isdigit(s2[l2]))
+			{
+				free(r);
+				printf("Error\n"), exit(98);
+			}
+			b = s2[l2] - '0';
+
+			c += r[l1 + l2 + 1] + (a * b);
+			r[l1 + l2 + 1] = c % 10;
+
+			c /= 10;
+		}
+		if (c)
+			r[l1 + l2 + 1] += c;
+	}
+	return (r);
+}
+
+/**
+ * main - multiply two big number
+ * @argc: the number of arguments
+ * @argv: the argument vector
+ * Return: Always 0
+ */
+int main(int argc, char **argv)
+{
+	char *r;
+	int a, c, x;
+
+	if (argc != 3)
+		printf("Error\n"), exit(98);
+
+	x = _strlen(argv[1]) + _strlen(argv[2]);
+	r = big_multiply(argv[1], argv[2]);
+	c = 0;
+	a = 0;
+	while (c < x)
+	{
+		if (r[c])
+			a = 1;
+		if (a)
+			_putchar(r[c] + '0');
+		c++;
+	}
+	if (!a)
+		_putchar('0');
+	_putchar('\n');
+	free(r);
+	return (0);
 }
